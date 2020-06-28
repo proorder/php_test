22accc5d636abb5686166c49077704d48a3aab82
Gleb Glebov <gleb@nextype.ru>
2020-06-28 20:10:40 +0500

    Были перепутаны задания

--- task_1.4/index.php
+++ task_1.4/index.php
@@ -1,20 +1,18 @@
 <?php
 
-$array = [
-    [
-        'sort' => '20',
-        'name' => 'Mike'
-    ],
-    [
-        'sort' => '10',
-        'name' => 'Adam'
-    ],
-    [
-        'sort' => '40',
-        'name' => 'Stive'
-    ],
-    [
-        'sort' => '300',
-        'name' => 'Jane'
-    ],
-];
+$filename = __DIR__ . "/data.json";
+$result = [];
+if (file_exists($filename))
+    $result = json_decode (file_get_contents ($filename), true);
+
+?>
+
+<? foreach ($result as $item): ?>
+<div>
+    <b><?=$item['name']?></b><br>
+    <small>Viewed: <?=$item['viewed']?></small><br>
+    <?=$item['text']?><br><br>
+    <a href="?set_viewed=<?=$item['id']?>">I watched</a>
+    <hr>
+</div>
+<? endforeach; ?>
